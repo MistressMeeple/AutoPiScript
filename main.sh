9cbc6e3cdd2287aface08494d654a50a6bd377b2
Mistress_Meeple <jskynol001@gmail.com>
2021-03-25 00:41:37 +0000

    Update main.sh

--- main.sh
+++ main.sh
@@ -3,12 +3,19 @@
 echo "Starting the default changes for Myriad servers"
 # Add the new user admin account
 while [ x$username = "x" ]; do
+
 read -p "Please enter the username you wish to create : " username
+
 if id -u $username >/dev/null 2>&1; then
+
 echo "User already exists"
+
 username=""
+
 fi
+
 done
+
 echo "Adding the $username account"
 useradd -G adm,dialout,cdrom,sudo,audio,video,plugdev,games,users,input,netdev,gpio,i2c,spi -m $username
 echo "Now to set password"
